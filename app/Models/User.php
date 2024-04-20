2c9bf370c94c5ce0fa45d28ac9329d638ec93820
Shatha-Asidah <90701007+Shatha-Asidah@users.noreply.github.com>
2024-04-21 00:51:56 +0300

    login register logout
    USER

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,34 +11,21 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    
+    protected $table = "users";
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    protected $fillable = ['first_name','last_name','user_type','email','password','phone','location','iD_card','personal_photo','property_deed','clean_record'];
+
+    protected $primaryKey = "id";
+    public $timestamps = true ;
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+ 
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('user_type');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone')->nullable();
+            $table->text('location')->nullable();
+            $table->text('iD_card')->nullable();
+            $table->text('personal_photo')->nullable();
+            $table->text('property_deed')->nullable();
+            $table->text('clean_record')->nullable();
             $table->timestamps();
         });
     }
